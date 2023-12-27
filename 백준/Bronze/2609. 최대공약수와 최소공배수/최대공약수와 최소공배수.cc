0d58ebc4b63ac135ba6d8770297ce4397e231522
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2023-12-27 17:18:48 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,14 +1,23 @@
+//유클리드 호제법 사용
 #include <iostream>
 #include <cassert>
 #define MAX 100
 
+int gcd(int a, int b)
+{
+    int c = a % b;
+    
+    while(c != 0) {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    return b;
+}
+
 int main(void)
 {
     int num1, num2;
-    int div1[MAX] = {0};
-    int div2[MAX] = {0};
-    int count1 = 0;
-    int count2 = 0;
     int max_div = 0;
     int min_mul = 0;
     
@@ -17,28 +26,7 @@ int main(void)
     assert(1 <= num1 && num1 <= 10000);
     assert(1 <= num2 && num2 <= 10000);
     
-    for (int i = 1; i <= num1; i++) {
-        if (num1 % i == 0) {
-            div1[count1] = i;
-            count1++;
-        }
-    }
-    
-    for (int j = 1; j <= num2; j++) {
-        if (num2 % j == 0) {
-            div2[count2] = j;
-            count2++;
-        }
-    }
-    
-    for (int k = 0; k < count1; k++) {
-        for (int q = 0; q < count2; q++) {
-            if (div1[k] == div2[q]) {
-                max_div = div1[k];
-            }
-        }
-    }
-    
+    max_div = gcd(num1, num2);
     min_mul = num1 * num2 / max_div;
     
     std::cout << max_div << '\n';
