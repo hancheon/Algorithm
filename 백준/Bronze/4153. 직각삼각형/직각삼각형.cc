400cee96e7f8b57905963e20aed6ace4f4a4e7c2
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2024-01-03 16:44:48 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+	int A, B, C;
+	int count = 0;
+
+	while (1) {
+		std::cin >>  A >> B >> C;
+
+		if (A == 0) {
+			break;
+		}
+
+		int A2 = pow(A, 2);
+		int B2 = pow(B, 2);
+		int C2 = pow(C, 2);
+
+		if (A2 == B2 + C2 || B2 == A2 + C2 || C2 == A2 + B2) {
+			std::cout << "right" << std::endl;
+		}
+		else {
+			std::cout << "wrong" << std::endl;
+		}
+
+	}
+
+	return 0;
+}
