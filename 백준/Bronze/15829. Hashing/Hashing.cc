adc9d15607a25076f184ea9e5dfafa41fde07cf5
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2024-01-05 12:55:13 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,28 +1,20 @@
 #include <iostream>
-#include <cmath>
-
-int hasing(int n, char s[])
-{
-	int sum = 0;
-
-	for (int i = 0; i < n; i++) {
-		sum += (s[i] - 96) * (int)pow(31, i);
-	}
-
-	return sum % 1234567891;
-}
 
 int main()
 {
 	int N;
-	char str[5] = { 0 };
+	char str[50] = { 0 };
+	long long sum = 0;
+	long long r = 1;
 	std::cin >> N;
 
 	for (int i = 0; i < N; i++) {
 		std::cin >> str[i];
+		sum = (sum + ((str[i] - 96) * r )) % 1234567891;
+		r = (r * 31) % 1234567891;
 	}
 
-	std::cout << hasing(N, str) << std::endl;
+	std::cout << sum << std::endl;
 
 	return 0;
 }
