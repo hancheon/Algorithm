8eecd681448876806f30d52a09e533968e000c24
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2024-10-31 15:18:53 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -5,57 +5,27 @@ using namespace std;
 int main()
 {
     string S; // 입력받을 문자열
-    int cnt = 0;
-    int zeroGroupCnt = 0; // 0 덩어리 개수
-    int oneGroupCnt = 0; // 1 덩어리 개수
+    int zeroCnt = 0; // 1을 0으로 바꾸는 횟수
+    int oneCnt = 0; // 0을 1로 바꾸는 횟수
     cin >> S;
 
-    for (int i = 0; i < S.size(); i++)
+    if (S[0] == '0') // 첫 문자가 0이면 1로 바꿈
+        oneCnt++;
+    else // 첫 문자가 1이면 0로 바꿈
+        zeroCnt++;
+
+    for (int i = 0; i < S.size() - 1; i++)
     {
-        if (S[i] == '0') // 0이면 다음 1 나올 때까지 한 덩어리로 묶기
+        if (S[i] != S[i + 1]) // i번째 문자와 다음 문자가 다를 때
         {
-            if (i == S.size() - 1)
-            {
-                zeroGroupCnt++;
-                break;
-            }
-            for (int j = i + 1; j < S.size(); j++)
-            {
-                if (S[j] == '1' || (j == S.size() - 1))
-                {
-                    zeroGroupCnt++;
-                    i = j - 1;
-                    break;
-                }
-            }
-        }
-        else // 1이면 다음 0 나올 때까지 한 덩어리로 묶기
-        {
-            if (i == S.size() - 1)
-            {
-                oneGroupCnt++;
-                break;
-            }
-            for (int j = i + 1; j < S.size(); j++)
-            {
-                if (S[j] == '0' || (j == S.size() - 1))
-                {
-                    oneGroupCnt++;
-                    i = j - 1;
-                    break;
-                }
-            }
+            if (S[i + 1] == '0') // 다음 문자가 0이면 1로 바꿈
+                oneCnt++;
+            else // 다음 문자가 1이면 0으로 바꿈
+                zeroCnt++;
         }
     }
 
-    if (zeroGroupCnt + oneGroupCnt == 1)
-        cnt = 0;
-    else if (zeroGroupCnt <= oneGroupCnt) // 0의 개수가 1의 개수보다 작거나 같은 경우
-        cnt += zeroGroupCnt;
-    else // 1의 개수가 0의 개수보다 작은 경우
-        cnt += oneGroupCnt;
-
-    cout << cnt << endl;
+    cout << min(zeroCnt, oneCnt) << endl;
 
     return 0;
 }
