b15476bc160e2ee0a899eeb06aecdb08247ed495
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-11 01:46:44 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -13,18 +14,7 @@ int main()
         cin >> num[cnt];
     }
     
-    for (int cnt = 0; cnt < 3; cnt++)
-    {
-        for (int cnt2 = cnt + 1; cnt2 < 3; cnt2++)
-        {
-            if (num[cnt] > num[cnt2])
-            {
-                int temp = num[cnt];
-                num[cnt] = num[cnt2];
-                num[cnt2] = temp;
-            }
-        }
-    }
+    sort(num, num+3);
     
     for (int cnt = 0; cnt < 3; cnt++)
         cout << num[cnt] << " ";
