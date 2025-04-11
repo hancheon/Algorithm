74c7a949c282a7335a2152bbc72fc67f32441ed7
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-11 11:53:30 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -1,44 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+string resStr = "DCBAE";
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int temp;
-    int result[3] = { 0 };
+    int input;
+    int result;
     
     for (int i = 0; i < 3; i++)
     {
+        result = 0;
         for (int j = 0; j < 4; j++)
         {
-            cin >> temp;
-            result[i] += temp;
+            cin >> input;
+            result += input;
         }
-    }
-    
-    for (int i = 0; i < 3; i++)
-    {
-        switch (result[i])
-        {
-            case 0:
-                cout << 'D';
-                break;
-            case 1:
-                cout << 'C';
-                break;
-            case 2:
-                cout << 'B';
-                break;
-            case 3:
-                cout << 'A';
-                break;
-            case 4:
-                cout << 'E';
-                break;
-        }
-        cout << '\n';
+        cout << resStr[result] << '\n';
     }
     
     return 0;
