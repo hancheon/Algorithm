449630f92e141707afb1ad533d9487cf6a7892c7
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-11 11:32:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -8,7 +8,6 @@ int main()
     cin.tie(nullptr);
     
     int num[4];
-    int cnt = 0;
     
     for (int i = 0; i < 3; i++)
     {
@@ -17,27 +16,10 @@ int main()
     
     sort(num, num + 3);
     
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = i+1; j < 3; j++)
-        {
-            if (num[i] == num[j])
-            {
-                num[3] = num[i];
-                cnt++;
-                break;
-            }
-        }
-    }
-    
-    if (cnt == 2)
-    {
-        cout << 10000 + num[3] * 1000;
-    }
-    else if (cnt == 1)
-    {
-        cout << 1000 + num[3] * 100;
-    }
+    if (num[0] ==  num[2])
+        cout << 10000 + num[0] * 1000;
+    else if (num[0] ==  num[1] || num[1] == num[2])
+        cout << 1000 + num[1] * 100;
     else 
         cout << num[2] * 100;
     
