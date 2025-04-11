65c90170020c14843f8b65f6f8fceae65ffebfa6
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-11 11:06:23 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int year;
+    cin >> year;
+    if (year % 4 == 0)
+    {
+        if (year % 100 != 0)
+            cout << 1;
+        else if (year % 400 == 0)
+            cout << 1;
+        else
+            cout << 0;
+    }
+    else
+        cout << 0;
+    
+    return 0;
+}
