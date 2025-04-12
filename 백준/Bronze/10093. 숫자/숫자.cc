533ab3a4f25b949f27fbea9299af32746b0cc15e
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-12 11:48:33 +0900

    [Bronze II] Title: 숫자, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int A, B, cnt;
+    cin >> A >> B;
+    
+    if (A == B)
+        cnt = 0;
+    else
+        cnt = abs(B - A) - 1;
+    
+    cout << cnt << '\n';
+    
+    if (A < B)
+    {
+        for (int i = 1; i <= cnt; i++)
+        {
+            cout << A + i << ' ';
+        }
+    }
+    else if (A > B)
+    {
+        for (int i = 1; i <= cnt; i++)
+        {
+            cout << B + i << ' ';
+        }
+    }
+    
+    return 0;
+}
