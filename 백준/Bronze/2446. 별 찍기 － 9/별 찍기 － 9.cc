78abe4775397d09d6413637edb38fc6d1baf1ffb
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-14 13:10:38 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    for (int i = 1; i <= 2 * N - 1; i++)
+    {
+        if (i <= N)
+        {
+            for (int j = 0; j < i - 1; j++)
+            {
+                cout << ' ';
+            }
+            for (int j = 0; j < 2 * (N - i) + 1; j++)
+            {
+                cout << '*';
+            }
+        }
+        else
+        {
+            for (int j = 0; j < 2 * N - i - 1; j++)
+            {
+                cout << ' ';
+            }
+            
+            for (int j = 0; j < 2 * (i - N) + 1; j++)
+            {
+                cout << '*';
+            }
+        }
+        
+        cout << '\n';
+    }
+    
+    return 0;
+}
