58b9bd2cc18a54c7563d530dc1ef5579301f0587
Yun JuYeon <69851118+hancheon@users.noreply.github.com>
2025-04-16 11:38:02 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -2,25 +2,18 @@
 #include <algorithm>
 using namespace std;
 
-bool cmp(pair<int, int> num1, pair<int, int> num2)
-{
-    return num1.second <= num2.second;
-}
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    pair<int, int> nums[9];
+    int num[9];
     
     for (int i = 0; i < 9; i++)
     {
-        nums[i].first = i;
-        cin >> nums[i].second;
+        cin >> num[i];
     }
     
-    sort(nums, nums + 9, cmp);
-    
-    cout << nums[8].second << '\n' << nums[8].first + 1;
+    cout << *max_element(num, num+9) << '\n';
+    cout << max_element(num, num+9) - num + 1;
 }
